963924ffffa100d0df88032a1d5cc44ef9c9507a
Brian Kang <briank@scribd.com>
2024-10-19 18:37:11 -0300

    Update env name

--- src/services/gitportfolio.ts
+++ src/services/gitportfolio.ts
@@ -1,8 +1,8 @@
-const VITE_GITPORTFOLIO_API_URL = import.meta.env.VITE_GITPORTFOLIO_API_URL || "http://localhost:4567"
+const REACT_APP_GITPORTFOLIO_API_URL = process.env.REACT_APP_GITPORTFOLIO_API_URL || "http://localhost:4567"
 
 export async function fetchDevType(file_tree: string[]) {
   const response = await fetch(
-    `${VITE_GITPORTFOLIO_API_URL}/api/v1/type`,
+    `${REACT_APP_GITPORTFOLIO_API_URL}/api/v1/type`,
     {
       method: "POST",
       headers: {
